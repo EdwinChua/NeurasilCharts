73d385332e8f71f0b77e3b3470b6fd2d12d5d639
Edwin <3dw1nchu4@gmail.com>
2022-06-29 11:53:11 +0800

    mapped chart on click to event emitter

--- projects/neurasil-charts/src/lib/neurasil-charts.component.ts
+++ projects/neurasil-charts/src/lib/neurasil-charts.component.ts
@@ -39,6 +39,8 @@ export class NeurasilChartsComponent implements OnInit, AfterViewInit, OnChanges
   @Output() swapLabelsAndDatasetsChange = new EventEmitter();
   @Input() globalFilter: string = "";
 
+  @Output() dataOnClick = new EventEmitter();
+
   toolbarProps = {
     chartType: this.chartType ? this.chartType : NEURASIL_CHART_TYPE.BAR,
     _datasetFilter: "",
@@ -67,14 +69,14 @@ export class NeurasilChartsComponent implements OnInit, AfterViewInit, OnChanges
   }
   ngOnChanges(changes: SimpleChanges) {
     if (changes) {
-      console.log(changes) 
+      // console.log(changes)
       this.drawChart();
     }
   }
 
   updateToolbarProps(ev) {
-    console.log(">>>" , ev)
-    console.log(this.toolbarProps)
+    // console.log(">>>", ev)
+    // console.log(this.toolbarProps)
     this.chartTypeChange.emit(this.toolbarProps.chartType);
     this.showToolbarChange.emit(this.showToolbar);
     this.swapLabelsAndDatasetsChange.emit(this.toolbarProps.swapLabelsAndDatasets)
@@ -94,13 +96,36 @@ export class NeurasilChartsComponent implements OnInit, AfterViewInit, OnChanges
       }
       filterString += this.toolbarProps._datasetFilter;
       let filteredData = this.neurasilDataFilter.transform(this.data, filterString);
-      console.log(filteredData);
       this.hasData = (filteredData && filteredData.length > 0);
       if (this.hasData) {
         let o = this.neurasilChartsService.parseDataFromDatasource(this.toolbarProps.chartType, filteredData, this.toolbarProps.swapLabelsAndDatasets);
         let props = this.neurasilChartsService.chartObjectBuilder(this.toolbarProps.chartType, o.data, this.useAltAxis, this.chartTitle, this.yAxisLabelText, this.yAxisLabelText_Alt, this.xAxisLabelText, o._cornerstone, this.toolbarProps.swapLabelsAndDatasets, o._formatObject);
         if (this.toolbarProps.chartType == NEURASIL_CHART_TYPE.STACKED_PARETO) {
           this.neurasilChartsService.performParetoAnalysis(props); // modify chart props object
+        }
+
+        let THIS = this;
+        //Cant put this in service, idk why
+        props.options.onClick = function (ev, element, chartObj) {
+          if (element[0]) {
+            let clickData = element[0];
+            let xAxisVal = props.data.labels[clickData.index];
+            let dataset = props.data.datasets[clickData.datasetIndex];
+            let datasetLabel = dataset.label;
+            let datasetVal = dataset.data[clickData.index];
+            let customDataObj = {
+              val: datasetVal,
+              dataLabel: THIS.swapLabelsAndDatasets ? datasetLabel : xAxisVal,
+              datasetLabel:THIS.swapLabelsAndDatasets ? xAxisVal : datasetLabel
+            }
+            let data = {
+              event: ev,
+              element: element,
+              chartInstance: chartObj,
+              data: customDataObj
+            }
+            THIS.dataOnClick.emit(data)
+          }
         }
         this._canvas = new Chart(ctx, props);
       }
